45be6ce765af8b65e3677d420495b9c96edcf2fd
Diego <diegobustos1229@gmail.com>
2021-01-17 19:33:40 -0800

    Add models and change UI

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,9 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Diego Bustos on 2021-01-17.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ BMI Calculator/Controllers/ResultsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ResultsViewController.swift
+//  BMI Calculator
+//
+//  Created by Diego Bustos on 2021-01-17.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    var bmiValue: String?
+    var bmiAdvice: String?
+    var bmiColor: UIColor?
+    
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bmiLabel.text = bmiValue
+        adviceLabel.text = bmiAdvice
+        
+        // Do any additional setup after loading the view.
+    }
+
+     @IBAction func recalculatPressed(_ sender: UIButton) {
+        
+        self.dismiss(animated: true, completion: nil)
+     }
+     
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    /*
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
