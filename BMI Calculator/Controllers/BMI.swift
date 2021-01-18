9d470057e5a08e7ce46d29df0da551d6a3941464
Diego <diegobustos1229@gmail.com>
2021-01-17 19:34:31 -0800

    add models

--- BMI Calculator/Controllers/BMI.swift
+++ BMI Calculator/Controllers/BMI.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+import UIKit
+
+
+struct BMI {
+    
+    let value: Float
+    let advice: String
+    let color: UIColor
+    
+}
--- BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import UIKit
+
+struct CalculatorBrain {
+    
+    var bmi: BMI?
+    
+    
+    mutating func calculateBMI(_ height: Double, _ weight: Double) {
+        let bmiValue = Float(String(format: "%.2f", weight / pow(height,2))) ?? 0.0
+        if bmiValue < 18.5{
+            bmi = BMI(value: bmiValue, advice: "Eat more pies", color: #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1))
+        }else if bmiValue < 24.9{
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1))
+        }else{
+            bmi = BMI(value: bmiValue, advice: "Eat less pies", color: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))
+        }
+    }
+    
+    func getBMIValue() -> String{
+        String(bmi?.value ?? 0.0)
+    }
+    
+    func getAdvice() -> String{
+        bmi?.advice ?? "Loading.."
+    }
+    
+    
+    func getColor() -> UIColor{
+        bmi?.color ?? #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+    }
+}
